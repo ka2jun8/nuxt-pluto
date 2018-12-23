b256e15baca705f2b5592253eb17b443e0433a20
ka2jun8 <ka210n8@gmail.com>
2018-12-23 12:41:29 +0900

    chore: remove unused value

--- server/services/index.js
+++ server/services/index.js
@@ -1,6 +1,4 @@
-const axios = require("axios");
 const AgreedSample = require("./AgreedSample");
-const API_HOST = "http://localhost:3010";
 
 async function apiGateway(req, res, next) {
   if (req.path === "/agreedsample") {
