0b2dada023d1a43ac6ea052faed4ba13f5f44b41
ka2jun8 <ka210n8@gmail.com>
2019-01-02 00:00:18 +0700

    feat: bar page

---
+++ pages/bar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div 
+      v-for="elm in elms" 
+      :key="elm">
+      Bar![{{ elm }}]
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      elms: new Array(500).fill(null).map((k, i) => i),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
